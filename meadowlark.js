a456ede2489a551d65f63f0c43a56939c95554dd
Dustin Yelland <dyelland@unitedfiregroup.com>
2021-10-22 19:21:58 -0500

    unit tests for route handlers

--- meadowlark.js
+++ meadowlark.js
@@ -1,32 +1,23 @@
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
-const fortune = require("./lib/fortune");
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+const handlers = require("./lib/handlers");
 
 app.engine("handlebars", expressHandlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => res.render("home"));
+app.get("/", handlers.home);
 
-app.get("/about", (req, res) => {
-  fortune.getFortune();
-  res.render("about", { fortune });
-});
+app.get("/about", handlers.about);
 
-app.use((req, res) => {
-  res.status(404);
-  res.render("404");
-});
+app.use(handlers.notFound);
 
-app.use((err, req, res, next) => {
-  console.error(err.message);
-  res.status(500);
-  res.render("500");
-});
+app.use(handlers.serverError);
 
 app.listen(port, () =>
   console.log(
