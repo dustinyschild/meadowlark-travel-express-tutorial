c048e68de6577089907e4d2d550b9249f7b94957
Dustin Yelland <dyelland@gmail.com>
2021-10-24 21:41:47 -0500

    add sections support with handlebars

--- meadowlark.js
+++ meadowlark.js
@@ -6,17 +6,29 @@ const port = process.env.PORT || 3000;
 
 const handlers = require("./lib/handlers");
 
-app.engine("handlebars", expressHandlebars({ defaultLayout: "main" }));
+app.engine(
+  "handlebars",
+  expressHandlebars({
+    defaultLayout: "main",
+    helpers: {
+      section: function (name, options) {
+        if (!this._sections) this._sections = {};
+        this._sections[name] = options.fn(this);
+        return null;
+      }
+    }
+  })
+);
+
 app.set("view engine", "handlebars");
 
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", handlers.home);
-
 app.get("/about", handlers.about);
+app.get("/section-test", handlers.sectionTest);
 
 app.use(handlers.notFound);
-
 app.use(handlers.serverError);
 
 // ask if this file is the main module, i.e. was I run directly from command line
