f8ac453d197e2fdea1427f6986a9f418c2b5cbfa
Dustin Yelland <dyelland@gmail.com>
2021-10-26 14:25:38 -0500

    file uploads

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
+const multiparty = require("multiparty");
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -40,6 +41,19 @@ app.get("/newsletter-signup/thank-you", handlers.newsletterSignupThankYou);
 
 app.get("/newsletter", handlers.newsletter);
 app.post("/api/newsletter-signup", handlers.api.newsletterSignup);
+
+app.get("/contest/vacation-photo", handlers.vacationPhotoContest);
+app.post("/contest/vacation-photo/:year/:month", (req, res) => {
+  const form = new multiparty.Form();
+  form.parse(req, (err, fields, files) => {
+    if (err) return res.status(500).send({ error: err.message });
+    handlers.vacationPhotoContestProcess(req, res, fields, files);
+  });
+});
+app.get(
+  "/contest/vacation-photo/thank-you",
+  handlers.vacationPhotoContestThankYou
+);
 
 app.use(handlers.notFound);
 app.use(handlers.serverError);
